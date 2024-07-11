8065654ea01db9e3b7992412729c9bd94ce9d3f8
Golden1636 <okeamagolden@yahoo.com>
2024-07-11 19:55:45 +0100

    implemented some fxns in the app/api/webhook/route.ts file e.g createUser() when the eventType from the clerk webhook is user.created i.e when a new user signs up in our app through Clerk

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,8 +2,16 @@
 
 import UserModel from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import QuestionModel from "@/database/question.model";
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
   try {
     connectToDatabase();
     console.log(params);
@@ -13,6 +21,72 @@ export async function getUserById(params: any) {
     const user = await UserModel.findOne({ clerkId: userId });
 
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await UserModel.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    // {new: true} returns the updated instance of the user
+    await UserModel.findOneAndUpdate({ clerkId }, updateData, { new: true });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    // find the user so we can have the reference 'user'
+    const user = await UserModel.findOneAndDelete({ clerkId });
+
+    // If no user with that id was found
+    if (!user) {
+      throw new Error("User not found");
+    }
+    // if a user with the id exists,
+    // Delete the user from the database
+    // Delete everything the user has ever done
+    // ---> questions, answers, comments, etc
+    // DELETING QUESTIONS
+    // get user question ids
+    const userQuestionIds = await QuestionModel.find({
+      author: user._id,
+    }).distinct("_id");
+
+    // delete user questions
+    await QuestionModel.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc
+
+    // delete user
+    const deletedUser = await UserModel.findByIdAndDelete(user._id);
+
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
