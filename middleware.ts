5258239c7637d6dbfca42c22f2d792d347a11081
Golden1636 <okeamagolden@yahoo.com>
2024-07-21 11:38:01 +0100

    getting the webhook secret from env file

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/ask-question(.*)",
-  // "/questions(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
