30dbb3cc7b43e5c05298fee81ff50d210bd21497
Golden1636 <okeamagolden@yahoo.com>
2024-07-21 12:56:12 +0100

    got the user's picture to show besides their name under their questions

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,13 +1,13 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const page = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  const userId = "1234567890";
+  // const userId = "1234567890";
   if (!userId) redirect("/sign-in");
 
   // USING OUR SERVER ACTION TO GET A USERDOCUMENT IN OUR users collection in our DB
