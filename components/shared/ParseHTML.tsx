b31a0c1720d223a491844562b6f9648c175821d4
Golden1636 <okeamagolden@yahoo.com>
2024-11-26 10:42:09 +0100

    prepare for deployment

--- components/shared/ParseHTML.tsx
+++ components/shared/ParseHTML.tsx
@@ -38,7 +38,7 @@ const ParseHTML = ({ data }: Props) => {
     // highlight all the content is perceived as code
     Prism.highlightAll();
   }, []);
-  return <div className="text-dark200_light900">{Parse(data)}</div>;
+  return <div className={`markdown w-full min-w-full`}>{Parse(data)}</div>;
 };
 
 export default ParseHTML;
